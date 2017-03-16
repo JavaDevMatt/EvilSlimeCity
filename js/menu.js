6883627561790931d5af951c7d1d30a906dd0282
Mateusz Kupilas <matt@javadevmatt.pl>
2017-03-16 15:09:33 +0100

    start text for mobile and desktop

--- js/menu.js
+++ js/menu.js
@@ -10,9 +10,15 @@ var menuState = {
 
 		monster2 = game.add.sprite(430, game.world.height-60, 'monster1');
 		monster2.animations.add('stand', [0, 1, 2], 5, true);
+
+		if (!game.device.desktop){
+			var startLabel = game.add.text(200, game.world.height-60, 'Tap screen to start', {font: '25px Arial', fill: '#ffffff'});
+		} else {
+			var startLabel = game.add.text(200, game.world.height-60, 'Press Space to start',{font: '25px Arial', fill: '#ffffff'});
+		}
 		
 	},
-	
+
 	update: function() {
 		monster1.animations.play('stand');
 		monster2.animations.play('stand');
