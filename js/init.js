8150a9fa6115993f9d54e913ae7f3c6087f78cc5
Mateusz Kupilas <matt@javadevmatt.pl>
2017-03-16 14:17:48 +0100

    added animated monsters to menu screen

--- js/init.js
+++ js/init.js
@@ -6,6 +6,9 @@ var initState = {
 			{font: '40px Courier', fill: '#fff'});
 
 		game.load.image('menu-background', 'assets/menu-background.png'); // http://opengameart.org/content/industrial-parallax-background
+	
+		game.load.spritesheet('monster1', 'assets/monster1.png', 30, 23); // http://opengameart.org/content/scifi-creature-tileset-mini-32x32-scifi-creature-icons 
+		game.load.spritesheet('monster2', 'assets/monster2.png', 30, 23); // http://opengameart.org/content/scifi-creature-tileset-mini-32x32-scifi-creature-icons 
 	},
 
 	
--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,19 @@ var menuState = {
 	create: function() {
 
 		game.add.sprite(0, 0, 'menu-background');
+
+
+		monster1 = game.add.sprite(160, game.world.height-60, 'monster1');
+		monster1.animations.add('stand', [0, 1, 2], 5, true);
+
+		monster2 = game.add.sprite(430, game.world.height-60, 'monster1');
+		monster2.animations.add('stand', [0, 1, 2], 5, true);
 		
+	},
+	
+	update: function() {
+		monster1.animations.play('stand');
+		monster2.animations.play('stand');
 	},
 
 };
