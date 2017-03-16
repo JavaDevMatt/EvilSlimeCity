1d60bf3fb717798ebd6fc536547cc64f559d7c32
Mateusz Kupilas <matt@javadevmatt.pl>
2017-03-16 14:14:14 +0100

    added menu background

--- js/init.js
+++ js/init.js
@@ -5,13 +5,14 @@ var initState = {
 		var loadingLabel = game.add.text(20, 150, 'loading game data...', 
 			{font: '40px Courier', fill: '#fff'});
 
+		game.load.image('menu-background', 'assets/menu-background.png'); // http://opengameart.org/content/industrial-parallax-background
 	},
 
 	
 	create: function() {
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		
-		// game.state.start('menu');
+		game.state.start('menu');
 	}
 
 };
--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,8 @@
 var menuState = {
 
 	create: function() {
+
+		game.add.sprite(0, 0, 'menu-background');
 		
 	},
 
