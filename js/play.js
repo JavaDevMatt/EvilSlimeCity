e25ef59b5291ad09faec616866e7114356aab2a2
Mateusz Kupilas <matt@javadevmatt.pl>
2017-03-20 15:52:48 +0100

    added player

--- js/play.js
+++ js/play.js
@@ -10,10 +10,35 @@ var playState = {
 		level = this.chooseLevel();
 
 		level.createBackground(game);
+
+		this.initPlayer();
+
+		cursors = game.input.keyboard.createCursorKeys();
+
+        game.camera.follow(player);
 	},
 
 	update: function() {  
+		// animations
+	 	player.animations.play('stand');
 
+	 	// controls
+	    if (cursors.left.isDown){
+	        player.body.velocity.x = -150;
+	    }
+	    else if (cursors.right.isDown){
+	        player.body.velocity.x = 150;
+	    }
 	},
+
+	initPlayer: function(){
+		player = game.add.sprite(level.playerStartingX, level.playerStartingY, 'monster1');
+		player.anchor.setTo(0.5,0.5);
+		player.animations.add('stand', [0, 1, 2], 5, true);
+		game.physics.arcade.enable(player);
+		player.body.bounce.y = 0.2;
+   		player.body.gravity.y = 300;
+        player.body.collideWorldBounds = true;
+	 },
 
 };
