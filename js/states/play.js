6a5af5aa35b65dd12d0a3255b6f8b582569ab2d7
Mateusz Kupilas <matt@javadevmatt.pl>
2017-03-28 15:02:59 +0200

    moved tnt logic to TntHanlder

--- js/states/play.js
+++ js/states/play.js
@@ -24,6 +24,7 @@ var playState = {
 		collisionsHandler = new CollisionsHandler();
 		juiceEmitters = new JuiceEmitters();
 		arrowBooster = new ArrowBooster();
+		tntHandler = new TntHandler();
 
 		level.createBackground(game);
 		level.addStartingText(game);
@@ -113,42 +114,7 @@ var playState = {
 	 },
 
 	 tntExplode: function(){
-	 	if(canTntExplode){
-	 		game.add.tween(tnt).to( { alpha: 0 }, 700, Phaser.Easing.Linear.None, true, 0, 1000, true);
-	 		game.sound.play('tnt')
-	 		canTntExplode = false;
-	 		var l1 = game.add.text(tnt.x + 11, tnt.y - 30, '3!', 
-                {font: '20px Courier', fill: '#fff'});
-
-	 		var l2, l3;
-
-	 		setTimeout(function(){
-	 			l1.kill();
-	 			l2 = game.add.text(tnt.x + 11, tnt.y - 30, '2!', 
-                {font: '20px Courier', fill: '#fff'});
-			}, 1000);
-
-			setTimeout(function(){
-	 			l2.kill();
-	 			l3 = game.add.text(tnt.x + 11, tnt.y - 30, '1!', 
-                {font: '20px Courier', fill: '#fff'});
-			}, 2000);
-
-		 	setTimeout(function(){
-		 		game.camera.shake(0.04, 2000, true);
-		 		l3.kill();
-
-				setTimeout(function(){
-		 			tnt.kill();
-				}, 1000);
-
-		 		switchFallers.forEachAlive(function(item) {
-        			item.body.immovable = false;
-				}, this);
-
-			}, 3000);
-	 	}
-	 		
+	 		tntHandler.explode(tnt);
 	 },
 
 	killPlayer: function(){
