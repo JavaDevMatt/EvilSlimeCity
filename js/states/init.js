9e66f4d9d9d9cebb20c6d90b586462a861c18104
Mateusz Kupilas <matt@javadevmatt.pl>
2017-04-12 16:09:19 +0200

    another mobile controls iteration

--- js/states/init.js
+++ js/states/init.js
@@ -25,7 +25,8 @@ var initState = {
 		game.load.image('trampoline', 'assets/img/trampoline.png');
 		game.load.image('arrow', 'assets/img/arrow.png');
 		game.load.image('tnt', 'assets/img/tnt.png');
-		game.load.image('mobile', 'assets/img/mobile.png');
+		game.load.image('mobile_direction', 'assets/img/mobile_direction.png');
+		game.load.image('mobile_jump', 'assets/img/mobile_jump.png');
 		game.load.spritesheet('lava', 'assets/img/lava.png', 56, 32);
 		game.load.spritesheet('lava2', 'assets/img/lava2.png', 252, 32);
 
