1beb83c2b3672e7dd97c17cb09f6bfa416ba1f98
Mateusz Kupilas <matt@javadevmatt.pl>
2017-05-03 07:19:05 +0200

    added dummy level8

--- js/levels/index.js
+++ js/levels/index.js
@@ -5,7 +5,9 @@ import {Level4} from './logic/level04'
 import {Level5} from './logic/level05'
 import {Level6} from './logic/level06'
 import {Level7} from './logic/level07'
+import {Level8} from './logic/level08'
 
 
 
-export{ Level1, Level2, Level3, Level4, Level5, Level6, Level7 }
+
+export{ Level1, Level2, Level3, Level4, Level5, Level6, Level7, Level8 }
