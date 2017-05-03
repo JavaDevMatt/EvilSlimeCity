5f91ed54bbf8aa5e551ac785fac96b2408f291a5
Mateusz Kupilas <matt@javadevmatt.pl>
2017-05-03 07:44:45 +0200

    level7 WIP

--- js/levels/structures/level07.js
+++ js/levels/structures/level07.js
@@ -1,21 +1,108 @@
-module.exports ={
-	platforms: [
-		{ x:0, y: 300, type: 'platform' },
-	],
-	redSlimes: [
-	],
-	fallers: [
-	],
-	slowFallers: [],
-	trampolines: [],
-	lava: [
-	{ x:141, y: 332, type: 'lava' },
-	{ x:191, y: 332, type: 'lava' },
-
-		{ x:254, y: 332, type: 'lava2' },
-		{ x:700, y: 332, type: 'lava2' }
-	],
-	tnt: [],
-	riders: [],
-	switchFallers: []
+module.exports = {
+    "world": {
+        "bounds": {
+            "x1": 0,
+            "x2": 1285,
+            "y1": 0,
+            "y2": 376
+        },
+        "sprites": [
+            {
+                "x": 0,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 640,
+                "y": 0,
+                "type": "game-background"
+            }
+        ]
+    },
+    "platforms": [
+        {
+            "x": 10,
+            "y": 188,
+            "type": "tower1",
+            "id": 921187827491
+        },
+        {
+            "x": 897,
+            "y": 172,
+            "type": "tower1",
+            "id": 305633980628
+        }
+    ],
+    "redSlimes": [],
+    "fallers": [
+        {
+            "x": 240,
+            "y": 246,
+            "type": "faller",
+            "id": 730571784922
+        },
+        {
+            "x": 362,
+            "y": 246,
+            "type": "faller",
+            "id": 1485644898367
+        },
+        {
+            "x": 462,
+            "y": 246,
+            "type": "faller",
+            "id": 1162542429998
+        },
+        {
+            "x": 562,
+            "y": 246,
+            "type": "faller",
+            "id": 1152082788542
+        },
+        {
+            "x": 662,
+            "y": 246,
+            "type": "faller",
+            "id": 259599152071
+        },
+        {
+            "x": 762,
+            "y": 246,
+            "type": "faller",
+            "id": 259599152071
+        }
+    ],
+    "slowFallers": [],
+    "trampolines": [],
+    "lava": [
+        {
+            "x": 143,
+            "y": 329,
+            "type": "lava2",
+            "id": 574319754650
+        },
+        {
+            "x": 639,
+            "y": 329,
+            "type": "lava2",
+            "id": 707990015164
+        },
+        {
+            "x": 393,
+            "y": 329,
+            "type": "lava2",
+            "id": 104295083245
+        }
+    ],
+    "tnt": [],
+    "switchFallers": [],
+    "riders": [],
+    "arrows": [
+        {
+            "x": 494,
+            "y": 217,
+            "type": "arrow",
+            "id": 893620932444
+        }
+    ]
 }
