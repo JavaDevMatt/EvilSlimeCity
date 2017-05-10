34d70c0f9ddcde3d636b40523f1016b46172b2b4
Mateusz Kupilas <matt@javadevmatt.pl>
2017-05-10 08:40:31 +0200

    level WIP

--- js/levels/structures/level08.js
+++ js/levels/structures/level08.js
@@ -24,56 +24,75 @@ module.exports = {
             "x": 0,
             "y": 300,
             "type": "platform",
-            "id": 801310340631
+            "id": 601927357145
         },
         {
             "x": 0,
             "y": 575,
             "type": "platform",
-            "id": 958521258141
+            "id": 1338658451970
         },
         {
             "x": 141,
             "y": 575,
             "type": "platform",
-            "id": 500788132934
+            "id": 816323959442
         },
         {
             "x": 282,
             "y": 575,
             "type": "platform",
-            "id": 1277141951477
+            "id": 56469586187
         },
         {
             "x": 423,
             "y": 575,
             "type": "platform",
-            "id": 241479363013
+            "id": 979515944637
         },
         {
             "x": 564,
             "y": 575,
             "type": "platform",
-            "id": 1111592443943
+            "id": 183988337246
         },
         {
             "x": 705,
             "y": 575,
             "type": "platform",
-            "id": 1196418256731
+            "id": 50676239578
         },
         {
             "x": 846,
             "y": 575,
             "type": "platform",
-            "id": 1274911733966
+            "id": 1382320553622
+        },
+        {
+            "x": 568, 
+            "y": 250,
+            "type": "faller"
         }
     ],
-    "redSlimes": [],
-    "fallers": [], 
+    "redSlimes": [
+        {
+            "x": 590,
+            "y": 229,
+            "type": "monster2"
+        }
+    ],
+    "fallers": [
+        
+    ],
     "slowFallers": [],
     "trampolines": [],
-    "lava": [],
+    "lava": [
+        {
+            "x": 499,
+            "y": 275,
+            "type": "lava2"
+        }
+    ],
     "tnt": [],
     "switchFallers": [],
     "riders": [],
