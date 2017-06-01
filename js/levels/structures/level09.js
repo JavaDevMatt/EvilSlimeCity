aa129800c91f393d4a86213ed8e080993934b2d0
Mateusz Kupilas <matt@javadevmatt.pl>
2017-06-01 15:33:16 +0200

    level9 WIP

--- js/levels/structures/level09.js
+++ js/levels/structures/level09.js
@@ -1,15 +1,64 @@
-module.exports ={
-	platforms: [
-		{ x:0, y: 300, type: 'platform' },
-	],
-	redSlimes: [
-	],
-	fallers: [
-	],
-	slowFallers: [],
-	trampolines: [],
-	lava: [],
-	tnt: [],
-	riders: [],
-	switchFallers: []
+module.exports = {
+    "world": {
+        "bounds": {
+            "x1": 0,
+            "x2": 885,
+            "y1": 0,
+            "y2": 376
+        },
+        "sprites": [
+            {
+                "x": 0,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 640,
+                "y": 0,
+                "type": "game-background"
+            }
+        ]
+    },
+    "platforms": [
+        {
+            "x": 435,
+            "y": 306,
+            "type": "platform",
+            "id": 374475154139
+        },
+        {
+            "x": 349,
+            "y": 228,
+            "type": "tower1",
+            "id": 37192324969
+        },
+        {
+            "x": 0,
+            "y": 230,
+            "type": "tower1",
+            "id": 706819637854
+        }
+    ],
+    "redSlimes": [],
+    "fallers": [
+        {
+            "x": 212,
+            "y": 224,
+            "type": "faller"
+        }
+    ],
+    "slowFallers": [],
+    "trampolines": [],
+    "lava": [
+        {
+            "x": 75,
+            "y": 348,
+            "type": "lava2",
+            "id": 984394176031
+        }
+    ],
+    "tnt": [],
+    "switchFallers": [],
+    "riders": [],
+    "arrows": []
 }
