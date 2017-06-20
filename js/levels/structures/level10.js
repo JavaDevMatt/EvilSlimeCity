b59169f1f54a75aae69221ffc926937e1e37d036
Mateusz Kupilas <matt@javadevmatt.pl>
2017-06-20 10:58:16 +0200

    lvl10 WIP

--- js/levels/structures/level10.js
+++ js/levels/structures/level10.js
@@ -1,15 +1,159 @@
-module.exports ={
-	platforms: [
-		{ x:0, y: 300, type: 'platform' },
-	],
-	redSlimes: [
-	],
-	fallers: [
-	],
-	slowFallers: [],
-	trampolines: [],
-	lava: [],
-	tnt: [],
-	riders: [],
-	switchFallers: []
+module.exports = {
+    "world": {
+        "bounds": {
+            "x1": 0,
+            "x2": 1500,
+            "y1": 0,
+            "y2": 376
+        },
+        "sprites": [
+            {
+                "x": 0,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 640,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 1280,
+                "y": 0,
+                "type": "game-background"
+            }
+        ]
+    },
+    "platforms": [
+        {
+            "x": 0,
+            "y": 300,
+            "type": "platform",
+            "id": 498143438936
+        },
+        {
+            "x": 1385,
+            "y": 176,
+            "type": "platform2",
+            "id": 1229188281280
+        },
+        {
+            "x": 1329,
+            "y": 176,
+            "type": "platform2",
+            "id": 58139707519
+        },
+        {
+            "x": 1386,
+            "y": 328,
+            "type": "tower1",
+            "id": 618576943102
+        },
+        {
+            "x": 1254,
+            "y": 318,
+            "type": "tower1",
+            "id": 436659850692
+        },
+        {
+            "x": 1127,
+            "y": 265,
+            "type": "tower1",
+            "id": 462921609149
+        }
+    ],
+    "redSlimes": [
+        {
+            "x": 1382,
+            "y": 134,
+            "type": "monster2",
+            "id": 1461553149229
+        },
+        {
+            "x": 1323,
+            "y": 291,
+            "type": "monster2"
+        }
+    ],
+    "fallers": [
+        {
+            "x": 30,
+            "y": 107,
+            "type": "faller",
+            "id": 1157617710839
+        },
+        {
+            "x": 130,
+            "y": 136,
+            "type": "faller",
+            "id": 93517751007
+        },
+        {
+            "x": 230,
+            "y": 164,
+            "type": "faller",
+            "id": 1261130391857
+        },
+        {
+            "x": 330,
+            "y": 197,
+            "type": "faller",
+            "id": 749197677572
+        },
+        {
+            "x": 430,
+            "y": 244,
+            "type": "faller",
+            "id": 667102113418
+        },
+        {
+            "x": 530,
+            "y": 287,
+            "type": "faller",
+            "id": 277829480241
+        },
+        {
+            "x": 630,
+            "y": 308,
+            "type": "faller",
+            "id": 511576606220
+        },
+        {
+            "x": 730,
+            "y": 287,
+            "type": "faller",
+            "id": 388618147895
+        },
+        {
+            "x": 830,
+            "y": 254,
+            "type": "faller",
+            "id": 381438021701
+        },
+        {
+            "x": 930,
+            "y": 197,
+            "type": "faller",
+            "id": 237412817564
+        },
+        {
+            "x": 1030,
+            "y": 164,
+            "type": "faller",
+            "id": 284346313516
+        }
+    ],
+    "slowFallers": [],
+    "trampolines": [],
+    "lava": [],
+    "tnt": [],
+    "switchFallers": [],
+    "riders": [],
+    "arrows": [
+        {
+            "x": 1470,
+            "y": 296,
+            "type": "arrow"
+        }
+    ]
 }
