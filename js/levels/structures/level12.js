baef71ae9f76e588a2da4f537962e8d5ab902394
Mateusz Kupilas <matt@javadevmatt.pl>
2017-07-03 22:12:39 +0200

    lvl12 WIP

--- js/levels/structures/level12.js
+++ js/levels/structures/level12.js
@@ -2,7 +2,7 @@ module.exports = {
     "world": {
         "bounds": {
             "x1": 0,
-            "x2": 885,
+            "x2": 1278,
             "y1": 0,
             "y2": 376
         },
@@ -19,13 +19,48 @@ module.exports = {
             }
         ]
     },
-    "platforms": [],
+    "platforms": [
+        {
+            "x": 509,
+            "y": 289,
+            "type": "tower1",
+            "id": 110906774026
+        },
+        {
+            "x": 641,
+            "y": 269,
+            "type": "tower1",
+            "id": 279403564131
+        },
+        {
+            "x": 778,
+            "y": 249,
+            "type": "tower1",
+            "id": 1264054172610
+        },
+        {
+            "x": 909,
+            "y": 230,
+            "type": "tower1"
+        },
+        {
+            "x": 1148,
+            "y": 111,
+            "type": "faller"
+        }
+    ],
     "redSlimes": [
         {
             "x": 360,
-            "y": 100,
+            "y": 100, 
             "type": "monster2",
+            "id": 202400311828
         },
+        {
+            "x": 1172,
+            "y": 79,
+            "type": "monster2"
+        }
     ],
     "fallers": [],
     "slowFallers": [
@@ -33,7 +68,7 @@ module.exports = {
             "x": 379,
             "y": 320,
             "type": "faller",
-            "id": 1493951447256
+            "id": 1157755160076
         }
     ],
     "trampolines": [
@@ -41,18 +76,30 @@ module.exports = {
             "x": 401,
             "y": 247,
             "type": "trampoline",
-            "id": 1190398022126
+            "id": 1306818571338
+        },
+        {
+            "x": 988,
+            "y": 204,
+            "type": "trampoline"
         }
     ],
     "lava": [
         {
             "x": 0,
             "y": 342,
-            "type": "lava2"
+            "type": "lava2",
+            "id": 186195855101
         },
         {
             "x": 252,
             "y": 342,
+            "type": "lava2",
+            "id": 323519273520
+        },
+        {
+            "x": 1026,
+            "y": 346,
             "type": "lava2"
         }
     ],
@@ -63,14 +110,14 @@ module.exports = {
             "x": 80,
             "y": 300,
             "type": "raider",
-            "id": 492057308254
+            "id": 168321116176
         },
-
         {
             "x": 380,
             "y": 140,
             "type": "raider",
-        },
+            "id": 1090790259240
+        }
     ],
     "arrows": []
 }
