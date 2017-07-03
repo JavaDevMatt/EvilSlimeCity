d1a2642c70ea8a02d0e1f0fe97c14b7c6ba60323
Mateusz Kupilas <matt@javadevmatt.pl>
2017-07-03 19:08:13 +0200

    lvl11 WIP

--- js/levels/structures/level11.js
+++ js/levels/structures/level11.js
@@ -21,28 +21,10 @@ module.exports = {
     },
     "platforms": [
         {
-            "x": 0,
-            "y": 300,
-            "type": "platform",
-            "id": 776308714498
-        },
-        {
-            "x": 140,
-            "y": 300,
-            "type": "platform",
-            "id": 827366771393
-        },
-        {
-            "x": 281,
-            "y": 300,
-            "type": "platform",
-            "id": 482110753706
-        },
-        {
-            "x": 383,
-            "y": 244,
+            "x": 690,
+            "y": 191,
             "type": "tower1",
-            "id": 738042809590
+            "id": 317767747930
         }
     ],
     "redSlimes": [
@@ -50,31 +32,85 @@ module.exports = {
             "x": 233,
             "y": 276.4222222222222,
             "type": "monster2",
-            "id": 1446651998947
+            "id": 17342011327
+        },
+        {
+            "x": 756,
+            "y": 164,
+            "type": "monster2",
+            "id": 824798391200
         }
     ],
     "fallers": [],
     "slowFallers": [],
-    "trampolines": [],
+    "trampolines": [
+        {
+            "x": 477,
+            "y": 227,
+            "type": "trampoline",
+            "id": 763668340740
+        }
+    ],
     "lava": [
         {
             "x": 1,
             "y": 348,
-            "type": "lava2"
+            "type": "lava2",
+            "id": 1466044747671
         },
         {
             "x": 253,
             "y": 348,
-            "type": "lava2"
+            "type": "lava2",
+            "id": 823460170396
         },
         {
             "x": 505,
             "y": 348,
-            "type": "lava2"
+            "type": "lava2",
+            "id": 1186247555376
+        },
+        {
+            "x": 705,
+            "y": 348,
+            "type": "lava2",
+            "id": 732653437234
         }
     ],
-    "tnt": [],
-    "switchFallers": [],
+    "tnt": [
+        {
+            "x": 71,
+            "y": 251,
+            "type": "tnt",
+            "id": 361128106918
+        }
+    ],
+    "switchFallers": [
+        {
+            "x": 0,
+            "y": 300,
+            "type": "platform",
+            "id": 936217715008
+        },
+        {
+            "x": 140,
+            "y": 300,
+            "type": "platform",
+            "id": 1058672546741
+        },
+        {
+            "x": 281,
+            "y": 300,
+            "type": "platform",
+            "id": 1028133489654
+        },
+        {
+            "x": 403,
+            "y": 244,
+            "type": "tower1",
+            "id": 737891217523
+        }
+    ],
     "riders": [],
     "arrows": []
 }
