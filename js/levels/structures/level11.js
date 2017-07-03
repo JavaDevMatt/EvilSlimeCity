44b0512500fe61c2904612000b97c8521cee2193
Mateusz Kupilas <matt@javadevmatt.pl>
2017-07-03 18:49:37 +0200

    lvl11 WIP

--- js/levels/structures/level11.js
+++ js/levels/structures/level11.js
@@ -1,15 +1,80 @@
-module.exports ={
-    platforms: [
-        { x:0, y: 300, type: 'platform' },
+module.exports = {
+    "world": {
+        "bounds": {
+            "x1": 0,
+            "x2": 885,
+            "y1": 0,
+            "y2": 376
+        },
+        "sprites": [
+            {
+                "x": 0,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 640,
+                "y": 0,
+                "type": "game-background"
+            }
+        ]
+    },
+    "platforms": [
+        {
+            "x": 0,
+            "y": 300,
+            "type": "platform",
+            "id": 776308714498
+        },
+        {
+            "x": 140,
+            "y": 300,
+            "type": "platform",
+            "id": 827366771393
+        },
+        {
+            "x": 281,
+            "y": 300,
+            "type": "platform",
+            "id": 482110753706
+        },
+        {
+            "x": 383,
+            "y": 244,
+            "type": "tower1",
+            "id": 738042809590
+        }
     ],
-    redSlimes: [
+    "redSlimes": [
+        {
+            "x": 233,
+            "y": 276.4222222222222,
+            "type": "monster2",
+            "id": 1446651998947
+        }
     ],
-    fallers: [
+    "fallers": [],
+    "slowFallers": [],
+    "trampolines": [],
+    "lava": [
+        {
+            "x": 1,
+            "y": 348,
+            "type": "lava2"
+        },
+        {
+            "x": 253,
+            "y": 348,
+            "type": "lava2"
+        },
+        {
+            "x": 505,
+            "y": 348,
+            "type": "lava2"
+        }
     ],
-    slowFallers: [],
-    trampolines: [],
-    lava: [],
-    tnt: [],
-    riders: [],
-    switchFallers: []
+    "tnt": [],
+    "switchFallers": [],
+    "riders": [],
+    "arrows": []
 }
