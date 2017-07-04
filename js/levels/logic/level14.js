918d9c2b90726bc758963046a9137396f2e63d92
Mateusz Kupilas <matt@javadevmatt.pl>
2017-07-04 19:30:40 +0200

    lvl 14 done

--- js/levels/logic/level14.js
+++ js/levels/logic/level14.js
@@ -3,17 +3,59 @@ import {LevelPrototype} from './levelPrototype'
 let gState = new GameState().state;
 let _ = require( "lodash" );
 
+let spectrumFlag = true;
+
+
 let lvl = require( "./../structures/level14.js" );
 export class Level14 extends LevelPrototype {
     constructor() {
         super();
         let protoLevel = _.cloneDeep( this.prototypeLevel );
         this.levelObj = _.merge( protoLevel, lvl );
+        spectrumFlag = true;
     }
 
     addStartingText(){
-   
+   		let levelLabel = game.add.text(20, 158, 'Run...',
+                        {font: '20px Courier', fill: '#fff'});
+                setTimeout(function(){
+                        levelLabel.kill();
+                }, 3000);
     }
+
+    handleSpecialLevelEvents(){
+		if(spectrumFlag && gState.player.x > 150){
+			spectrumFlag = false;
+        	game.sound.play('scary1'); 
+        	game.camera.shake(0.03, 4000, true);
+
+    		gState.envObjects.switchFallers.forEachAlive(function(item) {
+    			item.body.immovable = false;
+				}, this);
+
+			let levelLabel = game.add.text(100, 110, 'RUN!',
+                {font: '60px Courier', fill: '#fff'});
+        }
+    }
+
+
+    addEndingText(){
+        game.add.text(gState.player.x - 400, 100, 'Phew...',
+                {font: '40px Courier', fill: '#fff'});
+                game.add.text(gState.player.x - 400, 136, 'that was close',
+                {font: '20px Courier', fill: '#fff'});
+    }
+
+
+    handleRidersLogic(){
+        this.rider1 = gState.envObjects.riders.children[0];
+        if(this.rider1.x < 1550){
+            this.rider1.body.velocity.x = 100;
+        }
+
+     }
+
+
 
 }
 
--- js/levels/structures/level14.js
+++ js/levels/structures/level14.js
@@ -1,15 +1,291 @@
-module.exports ={
-    platforms: [
-        { x:0, y: 300, type: 'platform' },
+module.exports = {
+    "world": {
+        "bounds": {
+            "x1": 0,
+            "x2": 1955,
+            "y1": 0,
+            "y2": 376
+        },
+        "sprites": [
+            {
+                "x": 0,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 640,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 1280,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 1920,
+                "y": 0,
+                "type": "game-background"
+            },
+            {
+                "x": 30,
+                "y": -70,
+                "type": "big-green"
+            },
+            {
+                "x": 420,
+                "y": -70,
+                "type": "big-red"
+            },
+            {
+                "x": 840,
+                "y": -70,
+                "type": "big-green"
+            }
+        ]
+    },
+    "platforms": [
+        
     ],
-    redSlimes: [
+    "redSlimes": [
+        {
+            "x": 314,
+            "y": 273,
+            "type": "monster2",
+            "id": 195441831207
+        },
+        {
+            "x": 528,
+            "y": 274,
+            "type": "monster2",
+            "id": 884383967084
+        },
+        {
+            "x": 1908,
+            "y": 117,
+            "type": "monster2"
+        }
     ],
-    fallers: [
+    "fallers": [
+        
     ],
-    slowFallers: [],
-    trampolines: [],
-    lava: [],
-    tnt: [],
-    riders: [],
-    switchFallers: []
+    "slowFallers": [],
+    "trampolines": [],
+    "lava": [
+        {
+            "x": 0,
+            "y": 342,
+            "type": "lava2",
+            "id": 186195855101
+        },
+        {
+            "x": 252,
+            "y": 342,
+            "type": "lava2",
+            "id": 323519273520
+        },
+        {
+            "x": 504,
+            "y": 342,
+            "type": "lava2",
+            "id": 323519273520
+        },
+        {
+            "x": 756,
+            "y": 342,
+            "type": "lava2",
+            "id": 323519273520
+        }, 
+        {
+            "x": 850,
+            "y": 342,
+            "type": "lava2",
+            "id": 323519273520
+        },
+        {
+            "x": 1026, 
+            "y": 342,
+            "type": "lava2"
+        },
+        {
+            "x": 1196, 
+            "y": 342,
+            "type": "lava2"
+        },
+
+        {
+            "x": 1356, 
+            "y": 342,
+            "type": "lava2"
+        },
+        {
+            "x": 1500, 
+            "y": 342,
+            "type": "lava2"
+        },
+        {
+            "x": 1650, 
+            "y": 342,
+            "type": "lava2"
+        },
+        {
+            "x": 1800, 
+            "y": 342,
+            "type": "lava2"
+        }
+    ],
+    "tnt": [],
+    "switchFallers": [
+        {
+            "x": 0,
+            "y": 300,
+            "type": "platform",
+            "id": 432571238655
+        },
+        {
+            "x": 141,
+            "y": 300,
+            "type": "platform2",
+            "id": 954957581917
+        },
+        {
+            "x": 196,
+            "y": 300,
+            "type": "platform2",
+            "id": 986786819999
+        },
+        {
+            "x": 253,
+            "y": 300,
+            "type": "platform2",
+            "id": 777225567831
+        },
+        {
+            "x": 309,
+            "y": 300,
+            "type": "platform2",
+            "id": 284689203539
+        },
+        {
+            "x": 365,
+            "y": 300,
+            "type": "platform2",
+            "id": 361118140010
+        },
+        {
+            "x": 422,
+            "y": 300,
+            "type": "platform2",
+            "id": 1402193498274
+        },
+        {
+            "x": 478,
+            "y": 300,
+            "type": "platform2",
+            "id": 771977793295
+        },
+        {
+            "x": 535,
+            "y": 300,
+            "type": "platform2",
+            "id": 990477602718
+        },
+        {
+            "x": 592,
+            "y": 301,
+            "type": "platform2",
+            "id": 146464932458
+        },
+        {
+            "x": 648,
+            "y": 301,
+            "type": "platform2",
+            "id": 15127547710
+        },
+        {
+            "x": 705,
+            "y": 301,
+            "type": "platform2",
+            "id": 1150738518065
+        },
+        {
+            "x": 755,
+            "y": 301,
+            "type": "platform2",
+            "id": 990719344156
+        },
+        {
+            "x": 805,
+            "y": 301,
+            "type": "platform2",
+            "id": 448082475199
+        },
+        {
+            "x": 855,
+            "y": 301,
+            "type": "platform2",
+            "id": 930164503827
+        },
+        {
+            "x": 905,
+            "y": 301,
+            "type": "platform2",
+            "id": 47263509540
+        },
+        {
+            "x": 955,
+            "y": 301,
+            "type": "platform2",
+            "id": 1004375785558
+        },
+        {
+            "x": 1005,
+            "y": 301,
+            "type": "platform2",
+            "id": 806186775579
+        },
+        {
+            "x": 1055,
+            "y": 301,
+            "type": "platform2",
+            "id": 620534846092
+        },
+        {
+            "x": 1105,
+            "y": 301,
+            "type": "platform2",
+            "id": 255815879601
+        },
+        {
+            "x": 1155,
+            "y": 301,
+            "type": "platform2",
+            "id": 1028365468559
+        },
+        {
+            "x": 1205,
+            "y": 301,
+            "type": "platform2",
+            "id": 894154462177
+        },
+        {
+            "x": 1261,
+            "y": 278,
+            "type": "tower1"
+        }
+    ],
+    "riders": [
+        {
+            "x": 1969,
+            "y": 149,
+            "type": "raider"
+        }
+    ],
+    "arrows": [
+        {
+            "x": 1043,
+            "y": 250,
+            "type": "arrow"
+        }
+    ]
 }
